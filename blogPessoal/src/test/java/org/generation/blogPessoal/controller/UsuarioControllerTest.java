c4e675345daaf32b40a844875515b531360ee731
JuliaInoscencia <julia.inoscencia@gmail.com>
2022-02-02 16:12:54 -0300

    back-end - Add Teste de software JUnit 5 + H2

--- blogPessoal/src/test/java/org/generation/blogPessoal/controller/UsuarioControllerTest.java
+++ blogPessoal/src/test/java/org/generation/blogPessoal/controller/UsuarioControllerTest.java
@@ -38,9 +38,11 @@ public class UsuarioControllerTest {
 	@Order(1)
 	@DisplayName("Cadastrar usuário")
 	public void deveCadastrarUser() {
-		HttpEntity<Usuario> request = new HttpEntity<Usuario>(new Usuario("Melissa Lima", "mel@email.com.br", "123456"));
+		HttpEntity<Usuario> request = new HttpEntity<Usuario>(new Usuario(0L,"Melissa Lima", "mel@email.com.br", "123456"));
 		ResponseEntity<Usuario> response = testRestTemplate.exchange("/usuarios/cadastrar", HttpMethod.POST, request, Usuario.class);
+		
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		
 		assertEquals(request.getBody().getNome(), response.getBody().getNome());
 		assertEquals(request.getBody().getUsuario(), response.getBody().getUsuario());
 	
@@ -54,20 +56,19 @@ public class UsuarioControllerTest {
 		ResponseEntity<Usuario> response = testRestTemplate.exchange("/usuarios/cadastrar", HttpMethod.POST, request, Usuario.class);
 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
 	}
-	/*@Test
-	@Order()
+	@Test
+	@Order(3)
 	@DisplayName("Alterar um usuário")
 	public void deveAtualizarUmUsuario() {
 		Optional<Usuario> usuarioCreate = usuarioService.CadastrarUsuario(new Usuario("Janaina Lopes", "jana@email.com.br", "123456"));
-		Usuario usuarioUpdate = new Usuario(usuarioCreate.get().getId(),"Janaina Lopes", "jana@email.com.br", "123456");
+		Usuario usuarioUpdate = new Usuario(usuarioCreate.get().getId(),"Janaina Lopes Alterado", "jana@email.com.br", "123456");
 		HttpEntity<Usuario> request = new HttpEntity<Usuario>(usuarioUpdate);
-		ResponseEntity<Usuario> response = testRestTemplate.withBasicAuth("root", "root").exchange("/usuarios/atualizar", HttpMethod.PUT, request, Usuario.class);
+		ResponseEntity<Usuario> response = testRestTemplate.withBasicAuth("jana@email.com.br", "123456").exchange("/usuarios/atualizar", HttpMethod.PUT, request, Usuario.class);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(usuarioUpdate.getNome(), response.getBody().getNome());
-		assertEquals(usuarioUpdate.getUsuario(), response.getBody().getUsuario());
-	}*/
+	}
 	@Test
-	@Order(3)
+	@Order(4)
 	@DisplayName("Listar todos os usuários")
 	public void deveMostrarTodosUsuarios() {
 		usuarioService.CadastrarUsuario(new Usuario("Matias Santos", "matias@email.com.br", "123456"));
